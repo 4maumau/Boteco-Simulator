46d85e37edaeea542a2a3cd0048c3c5e62209536
Lucas Félix <lsfelix321@gmail.com>
2021-01-05 23:55:22 -0300

    Interagir com arredores scprits

---
+++ GameJaaj5/Assets/Scripts/InteragirArredores.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO.Compression;
+using UnityEngine;
+
+public class InteragirArredores : MonoBehaviour
+{
+    [SerializeField] private float raioAlcance;
+
+    private void Update()
+    {
+        var raycastHit2D = Physics2D.CircleCast(transform.position, raioAlcance, Vector2.up);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(transform.position,raioAlcance);
+    }
+}
