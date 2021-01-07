7c93942736f8d18f05496f05c79dc2fcf8b41fb4
Lucas Félix <lsfelix321@gmail.com>
2021-01-07 16:06:32 -0300

    Proximação de interação pronta

---
+++ GameJaaj5/Assets/Scripts/Objects/HighlightInteractable.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public abstract class HighlightInteractable : MonoBehaviour
+{
+    protected SpriteRenderer _spriteRenderer;
+    protected bool _canInteract;
+    
+    [SerializeField] private Material interactableMaterial;
+    [SerializeField] protected Material spriteDefault;
+    [SerializeField] protected GameObject player;
+    
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _canInteract = false;
+    }
+
+    private void Update()
+    {
+        if (_canInteract && Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            Interact();
+        }
+    }
+
+    protected abstract void Interact();
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        player = other.gameObject;
+        _spriteRenderer.material = interactableMaterial;
+        _canInteract = true;
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+        
+        _spriteRenderer.material = spriteDefault;
+        _canInteract = false;
+    }
+    
+}
---
+++ GameJaaj5/Assets/Scripts/Objects/TorreDeCerveja.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorreDeCerveja : HighlightInteractable
+{
+    protected override void Interact()
+    {
+        _canInteract = false;
+        _spriteRenderer.material = spriteDefault;
+        GetComponent<BoxCollider2D>().enabled = false;
+        transform.position = player.transform.position + (Vector3.up * 1);
+        transform.SetParent(player.transform);
+    }
+}
