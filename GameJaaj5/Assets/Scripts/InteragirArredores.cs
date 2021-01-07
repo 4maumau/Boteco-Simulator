daa9a1042e0a052383adfe2ddb8d53281cc2b729
Lucas Félix <lsfelix321@gmail.com>
2021-01-06 22:48:36 -0300

    detecção de objetos interagiveis nos arredores, inicio de outline

--- GameJaaj5/Assets/Scripts/InteragirArredores.cs
+++ GameJaaj5/Assets/Scripts/InteragirArredores.cs
@@ -6,14 +6,22 @@ public class InteragirArredores : MonoBehaviour
 {
     [SerializeField] private float raioAlcance;
 
+    public LayerMask targetMask;
+    
     private void Update()
     {
-        var raycastHit2D = Physics2D.CircleCast(transform.position, raioAlcance, Vector2.up);
+        var interactableObjects = Physics2D.OverlapCircleAll(transform.position, raioAlcance,targetMask);
+
+        for (int i = 0; i < interactableObjects.Length; i++)
+        {
+            Transform target = interactableObjects[i].transform;
+            Debug.Log("Pode interagir com: " + target.gameObject.name);
+
+        }
+
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.green;
-        Gizmos.DrawSphere(transform.position,raioAlcance);
     }
 }
